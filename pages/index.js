5825dc85f11f9803228c5523d56dd9140a695422
Mir-Fayek-Hossain <mirfayekhossain@gmail.com>
2023-06-04 14:07:27 +0600

    Notely aadded

--- pages/index.js
+++ pages/index.js
@@ -168,7 +168,7 @@ export default function Home() {
                     <div className="grid grid-cols-2 backdrop-blur-2xl overflow-x-hidden">
                         {details.map((data) => (
                              <div
-                                className=""
+                                className="relative w-full aspect-[4/2] "
                                 key={data.id}
                             >
                                 
