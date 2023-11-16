df61f97b4c1e9f1811f9d18ae95eef21a08777a3
Mir-Fayek-Hossain <mirfayekhossain@gmail.com>
2023-11-16 21:28:49 +0600

    Removes unwanted code

--- pages/mir-fayek-hossain-cv.jsx
+++ pages/mir-fayek-hossain-cv.jsx
@@ -1,4 +1,4 @@
-const Resume = () => {
+const mirfayekhossaincv = () => {
     return (
         <div>
             <iframe
@@ -10,4 +10,4 @@ const Resume = () => {
     );
 };
 
-export default Resume;
+export default mirfayekhossaincv;
